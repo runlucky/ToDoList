c1cecfcd48acbd6ba8854a25d1f854018be9c109
Kakeru Fukuda <stand.by.we@gmail.com>
2020-12-29 22:20:05 +0900

    ガワ作成

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by H5267 on 2020/12/29.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
