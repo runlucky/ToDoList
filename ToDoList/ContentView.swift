7bfd9dd63fd300651e59afa2440231add9dc5e48
Kakeru Fukuda <stand.by.we@gmail.com>
2020-12-30 22:11:56 +0900

    しかかり

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Item(true)
+            Item(true)
+            Item(false)
+            Item(false)
+
+        }
     }
 }
 
@@ -18,4 +23,32 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+internal struct Item: View {
+    private var checked: Bool
+
+    internal init(_ checked: Bool) {
+        self.checked = checked
+    }
+
+    internal var body: some View {
+        HStack {
+            ZStack {
+                Image(systemName: "square.fill")
+                    .foregroundColor(.white)
+                if checked {
+                    Image(systemName: "checkmark.square.fill")
+                        .foregroundColor(.blue)
+                }
+                Image(systemName: "square")
+                    .font(Font.headline.weight(.ultraLight))
+                    .foregroundColor(.gray)
+            }
+            Text("hogehoge")
+        }
+    }
+
+
 }
